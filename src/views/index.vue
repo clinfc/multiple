2ae4171754179e522153fb6a0df482ced28f5fa4
clin <39868400+clinfc@users.noreply.github.com>
2020-07-04 17:20:50 +0800

    修改主页布局

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,58 +1,32 @@
 <template>
-  <div class="index">
-    <div class="header">
-      <form class="form">
-        <template v-for="item in type">
-          <label class="radio-label" :key="item.value">
-            <input type="radio" v-model="tv" name="type" :value="item.value"><span>{{item.name}}</span>
-          </label>
+  <div class="home-page">
+    <div class="card" v-for="row in data" :key="row.title">
+      <div class="header">{{row.title}}</div>
+      <div class="main">
+        <template v-for="ct in row.content">
+          <router-link class="item" :key="ct.name" :to="ct.route">{{ct.name}}</router-link>
         </template>
-      </form>
-    </div>
-    <div class="main">
-      <template v-for="item in type">
-        <div class="content" :key="item.value" v-if="tv === item.value">
-          <router-link class="card" v-for="ct in item.content" :key="ct.href" :to="ct.href">
-            <template v-if="ct.src">
-              <img :src="ct.src" :alt="ct.name">
-            </template>
-            <template e-else>
-              <div>{{ct.name}}</div>
-            </template>
-          </router-link>
-        </div>
-      </template>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'index',
   data() {
     return {
-      tv: 'CSS3',
-      type: [
+      data: [
         {
-          name: 'CSS3',
-          value: 'CSS3',
+          title: 'CSS',
           content: [
-            { name: 'Flex 布局', href: '/css/flex', src: '' },
+            { name: 'Flex 调试器', route: '/css/flex' }
           ]
         },
         {
-          name: '数据可视化',
-          value: 'visual',
+          title: '数据可视化',
           content: [
-            { name: '混合实例', href: '/visual/basics', src: '' },
-            { name: 'ECharts', href: '/echarts', src: '' },
-          ]
-        },
-        {
-          name: '测试',
-          value: 'test',
-          content: [
-            { name: 'test', href: '/test', src: '' }
+            { name: '混合案例', route: '/visual/basics' },
+            { name: 'ECharts', route: '/echarts' }
           ]
         }
       ]
@@ -61,88 +35,39 @@ export default {
 }
 </script>
 
-<style lang="scss">
-.index {
+<style lang="scss" scoped>
+.home-page {
   width: 1200px;
-  height: 100%;
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-
-  .header {
-    width: 100%;
-    height: 60px;
-    flex-shrink: 0;
-    line-height: 60px;
-    overflow: hidden;
-    border-bottom: 1px solid #e5e5e5;
-    .form {
-      width: 100%;
-      display: flex;
-      overflow-y: hidden;
-      overflow-x: scroll;
-      .radio-label {
-        flex-shrink: 0;
-        display: block;
-        cursor: pointer;
-        box-sizing: border-box;
-        input[type=radio] {
-          display: none;
-        }
-        span {
-          display: block;
-          padding: 0 15px;
-          border-bottom: 2px solid #FFF;
-          box-sizing: border-box;
-          font-size: 14px;
-          height: 60px;
-        }
-        input[type=radio]:checked + span {
-          color: #5FB878;
-          border-bottom-color: #5FB878;
-        }
-      }
+  margin: auto;
+  .card {
+    margin-top: 20px;
+    .header {
+      padding: 15px 10px;
+      font-size: 24px;
+      color: #333;
+      border-bottom: 1px solid #EAEAEA;
     }
-  }
-
-  .main {
-    flex: 1;
-    overflow: hidden;
-    .content {
-      width: 100%;
-      overflow-y: hidden;
+    .main {
+      padding: 15px 0;
       display: flex;
       flex-wrap: wrap;
-      padding: 0 5px 25px;
-      .card {
-        display: block;
-        width: 218px;
+      align-content: flex-start;
+      .item {
+        display: inline-block;
+        width: 210px;
+        height: 70px;
+        line-height: 70px;
         text-decoration: none;
-        border: 1px solid #5FB878;
+        color: #5fb878;
+        padding: 0 10px;
+        margin: 10px;
+        border: 1px solid #5fb878;
         border-radius: 5px;
-        box-shadow: 0 1px 10px 1px rgba(95, 184, 120, 0.5);
-        box-sizing: border-box;
-        margin-top: 25px;
-        overflow: hidden;
         &:hover {
-          box-shadow: 0 0 10px 1px #5FB878;
-        }
-        &:not(:nth-child(5n)) {
-          margin-right: 25px;
-        }
-        &>div {
-          padding: 20px;
-        }
-        &>img {
-          width: 100%;
-          height: 145.3px;
+          box-shadow: 0 0 7px 0 #5fb878;
         }
       }
     }
   }
-
 }
 </style>
