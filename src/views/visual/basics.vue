84c4763bba39bae7434ad7fd6d58c277b4d486ee
clin <39868400+clinfc@users.noreply.github.com>
2020-07-04 14:46:05 +0800

    Update basics.vue

--- src/views/visual/basics.vue
+++ src/views/visual/basics.vue
@@ -4,7 +4,7 @@
     <basics-sales></basics-sales>
     <basics-bottom></basics-bottom>
     <basics-map></basics-map>
-    <github :path="$route.path" home></github>
+    <toolbox home></toolbox>
   </div>
 </template>
 
