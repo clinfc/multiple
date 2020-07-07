23bd9f692201a626fb4c978412db6f149ff58451
clin <39868400+clinfc@users.noreply.github.com>
2020-07-07 11:18:35 +0800

    Update component.js

--- src/plugins/component.js
+++ src/plugins/component.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 
 import Toolbox from '@/components/toolbox.vue'
-import Github from '@/components/github.vue'
 
 Vue.component('toolbox', Toolbox)
-Vue.component('github', Github)
+Vue.component('github', () => import('@/components/github.vue'))
